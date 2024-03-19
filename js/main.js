7b0902f30be900f5e03c375e1d31ce0bd77adca5
Jose <jacastro023@gmail.com>
2024-03-19 13:36:31 -0500

    created state variables

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 
 
 /*----- state variables -----*/
-
+let board; // array of 7 column arrays
+let turn; // 1 or -1
+let winner; // null = no winner; 1 or -1 = winner; "T" = tie
 
 /*----- cached elements  -----*/
 
